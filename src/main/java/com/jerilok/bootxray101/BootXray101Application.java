7dc311266c2b604fe2afb7125fe89f0e64c4dfeb
Eric Jalal <eric.jalal@elisa.fi>
2021-05-14 23:53:50 +0300

    Init

---
+++ src/main/java/com/jerilok/bootxray101/BootXray101Application.java
@@ -0,0 +1,13 @@
+package com.jerilok.bootxray101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootXray101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootXray101Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jerilok/bootxray101/BootXray101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerilok.bootxray101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootXray101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
